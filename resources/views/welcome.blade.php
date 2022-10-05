1a1c50be9f4d68adbc9ef99b80e65ed6dd8c679c
webgently <oleksandrshkabura811@gmail.com>
2022-10-06 00:13:13 +0800

    feat: add global background color and import logo img and font-family to the project

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,11 +2,17 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link
+            href="https://fonts.googleapis.com/css2?family=Montserrat:wght@500;600&family=Mulish:wght@500&display=swap"
+            rel="stylesheet"
+        />
 
-        <title>Laravel 9 with vue 3 vite</title>
+        <title>Recipe manage</title>
         @vite('resources/css/app.css')
     </head>
-    <body>
+    <body class="bg-gray-50 dark:bg-gray-900 min-h-100vh">
         <div id="app"></div>
         @vite('resources/js/app.js')
     </body>
