dba11bc69b817b52ffcdc3c4ece505f994388d66
webgently <oleksandrshkabura811@gmail.com>
2022-10-06 02:20:35 +0800

    feat: add navbar ui and add login out function

--- resources/js/components/Home.vue
+++ resources/js/components/Home.vue
@@ -1,5 +1,60 @@
 <template>
-    <h1 class="text-3xl font-bold underline">
-        Hello world!
-    </h1>
+    <nav class="bg-gray-800 fixed top-0 left-0 right-0 z-10">
+        <div class="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
+            <div class="relative flex h-16 items-center justify-between">
+                <div class="flex flex-1 items-center justify-start">
+                    <div class="flex flex-shrink-0 items-center">
+                        <img class="block h-8 w-auto" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=500" alt="Your Company">
+                    </div>
+                </div>
+
+                <div class="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
+                    <button type="button" class="rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
+                        <span class="sr-only">View notifications</span>
+                        <svg class="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M14.857 17.082a23.848 23.848 0 005.454-1.31A8.967 8.967 0 0118 9.75v-.7V9A6 6 0 006 9v.75a8.967 8.967 0 01-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 01-5.714 0m5.714 0a3 3 0 11-5.714 0" />
+                        </svg>
+                    </button>
+
+                    <!-- Profile dropdown -->
+                    <div class="relative ml-3">
+                        <div>
+                            <button type="button" @click="handleUser" class="flex rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
+                                <img class="h-8 w-8 rounded-full" src="https://img.freepik.com/free-psd/3d-illustration-person-tank-top_23-2149436199.jpg" alt="">
+                            </button>
+                        </div>
+                        <div class="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1
+                            shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none" :class="{'hidden': isActive}" tabindex="-1">
+                            <button @click="logOut" class="block px-4 py-2 text-sm text-gray-700" role="menuitem" tabindex="-1" id="user-menu-item-2">Sign out</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </nav>
 </template>
+
+<script>
+
+export default {
+    name: 'Home',
+    data() {
+        return {
+            isActive: true,
+        };
+    },
+    created() {
+
+    },
+    methods: {
+        handleUser() {
+            this.isActive ? this.isActive = false : this.isActive = true;
+        },
+
+        logOut() {
+            this.$store.dispatch('auth/logout');
+            this.$router.push('/login');
+        },
+    },
+}
+</script>
