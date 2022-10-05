95d4b9bf030dc17f02209f530fc6d45f914ba28d
webgently <oleksandrshkabura811@gmail.com>
2022-10-06 02:08:05 +0800

    feat: complete register ui

---
+++ resources/js/components/Register.vue
@@ -0,0 +1,95 @@
+<template>
+    <div class="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
+        <a href="#" class="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white">
+            <img src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-login-form/draw2.webp"
+                 class="w-full w-40 h-32" alt="logo" />
+        </a>
+        <div class="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+            <div class="p-6 space-y-4 md:space-y-6 sm:p-8">
+                <h1 class="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
+                    Create and account
+                </h1>
+                <Form @submit="handleRegister" :validation-schema="schema" class="space-y-4 md:space-y-6">
+                    <div v-if="!successful">
+                        <div class="pt-6">
+                            <label for="username" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Username</label>
+                            <Field name="username" type="text" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name" />
+                            <ErrorMessage name="username" class="absolute text-sky-400 lg:text-sm md:text-sm text-xs pt-1" />
+                        </div>
+                        <div class="pt-6">
+                            <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Email</label>
+                            <Field name="email" type="email" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@company.com" />
+                            <ErrorMessage name="email" class="absolute text-sky-400 lg:text-sm md:text-sm text-xs pt-1" />
+                        </div>
+                        <div class="pt-6">
+                            <label for="password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
+                            <Field type="password" name="password" placeholder="••••••••" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" />
+                            <ErrorMessage name="password" class="absolute text-sky-400 lg:text-sm md:text-sm text-xs pt-1" />
+                        </div>
+                        <button :disabled="loading" class="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 mt-12">
+                            <span v-show="loading" class="spinner-border spinner-border-sm"></span>
+                            Create an account
+                        </button>
+                        <p class="text-sm font-light text-gray-500 dark:text-gray-400 mt-2">
+                            Already have an account?
+                            <router-link to="/login" class="font-medium hover:underline">Login here</router-link>
+                        </p>
+                    </div>
+                </Form>
+                <div v-if="message" class="alert" :class="successful ? 'alert-success' : 'alert-danger'">
+                    {{ message }}
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+import { Form, Field, ErrorMessage } from 'vee-validate';
+import * as yup from 'yup';
+
+export default {
+    name: 'REGISTER',
+    components: {
+        Form,
+        Field,
+        ErrorMessage,
+    },
+    data() {
+        const schema = yup.object().shape({
+            username: yup
+                .string()
+                .required('Username is required!')
+                .min(3, 'Must be at least 3 characters!')
+                .max(20, 'Must be maximum 20 characters!'),
+            email: yup
+                .string()
+                .required('Email is required!')
+                .email('Email is invalid!')
+                .max(50, 'Must be maximum 50 characters!'),
+            password: yup
+                .string()
+                .required('Password is required!')
+                .min(6, 'Must be at least 6 characters!')
+                .max(40, 'Must be maximum 40 characters!'),
+        });
+
+        return {
+            successful: false,
+            loading: false,
+            message: '',
+            schema,
+        };
+    },
+    computed: {
+        loggedIn() {
+            return this.$store.state.auth.status.loggedIn;
+        },
+    },
+    mounted() {
+        if (this.loggedIn) {
+            this.$router.push('/home');
+        }
+    },
+};
+</script>
